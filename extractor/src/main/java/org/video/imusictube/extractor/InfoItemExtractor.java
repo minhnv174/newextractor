96cdca68af56ad8f6475c01ed15f83c5124ffca6
minhnv <minhnguyenvan@vccorp.vn>
2019-08-30 11:53:22 +0700

    delete

--- extractor/src/main/java/org/video/imusictube/extractor/InfoItemExtractor.java
+++
@@ -1,9 +0,0 @@
-package org.video.imusictube.extractor;
-
-import org.video.imusictube.extractor.exception.ParsingException;
-
-public interface InfoItemExtractor {
-    String getName() throws ParsingException;
-    String getUrl() throws ParsingException;
-    String getThumbnailUrl() throws ParsingException;
-}
